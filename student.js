eb80a38babfc5954627aecf0280ecf8cd8216279
AtharvaYR <87003833+AtharvaYR@users.noreply.github.com>
2021-08-09 09:43:30 +0300

    Add files via upload

---
+++ student.js
@@ -0,0 +1,18 @@
+
+class Student
+{
+ constructor(n,a,g){
+this.name=n
+this.age=a;
+this.grade=g;
+
+ }  
+display(){
+console.log(this.name);
+console.log(this.age);
+console.log(this.grade);
+}
+}
+
+
+
